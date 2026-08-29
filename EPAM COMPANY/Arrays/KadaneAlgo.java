5a6d29a87670605151c77cde40bbfcd5965323d2
PRAVESH TIWARI <pravesht252@gmail.com>
2026-08-29 10:12:53 +0530

    empam

--- EPAM COMPANY/Arrays/KadaneAlgo.java
+++ EPAM COMPANY/Arrays/KadaneAlgo.java
@@ -1,7 +1,18 @@
 // package EPAM COMPANY.Arrays;
 
+
+// Time complexity O(n)
+// maximum subarray
 public class KadaneAlgo {
     public static void main(String[] args) {
-        int arr[]={};
+        int arr[]={-2,1,-3,4,-1,2,1,-5};
+        int currsum=arr[0];
+        int maxsum=arr[0];
+
+        for(int i=1;i<arr.length;i++){
+            currsum=Math.max(currsum,currsum+arr[i]);
+            maxsum=Math.max(maxsum, currsum);
+        }
+        System.out.println("Maximum subarray: "+maxsum);
     }
 }
